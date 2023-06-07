f65607b50311367df46682ac5388c2eddfe80f4d
Alexandre Daoud <alex.daoud@mac.com>
2023-06-07 13:02:33 -0700

    submit tests

---
+++ guanaco_python_package/src/chai_guanaco/submit.py
@@ -0,0 +1,9 @@
+import requests
+
+SUBMISSION_URL = "http://submit.chai-research.com"
+
+def submit_model(model_submission):
+    response = requests.post(url=SUBMISSION_URL, json=model_submission)
+    assert response.status_code == 200, response.json()["error"]
+
+    return {"submission_id": "name_123456"}
