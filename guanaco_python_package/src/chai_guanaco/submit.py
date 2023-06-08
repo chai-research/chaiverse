fe286c5417793b3119e876cb4df47d33b58b5d60
Alexandre Daoud <alex@seamlessml.com>
2023-06-08 23:58:51 +0100

    Fix test

--- guanaco_python_package/src/chai_guanaco/submit.py
+++ guanaco_python_package/src/chai_guanaco/submit.py
@@ -8,7 +8,7 @@ SUBMISSION_URL = os.path.join(BASE_URL, SUBMISSION_ENDPOINT)
 
 def submit_model(model_submission):
     submission_url = get_submission_url()
-    response = requests.post(submission_url, json=model_submission)
+    response = requests.post(url=submission_url, json=model_submission)
     assert response.status_code == 200, response.json()["error"]
     return response.json()
 
