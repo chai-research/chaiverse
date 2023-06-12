fe8aab434bec1e2261527c873dc770a7973fba2d
Valentin Assassi <valentin@seamlessml.com>
2023-06-12 15:58:50 +0000

    Deploy model

---
+++ guanaco_python_package/tests/test_submit.py
@@ -0,0 +1,57 @@
+from mock import patch
+
+import pytest
+
+from chai_guanaco.submit import submit_model, SUBMISSION_URL
+
+
+@pytest.fixture(autouse="session")
+def mock_post():
+    with patch("chai_guanaco.submit.requests.post") as func:
+        func.return_value.status_code = 200
+        func.return_value.json.return_value = {"submission_id": "name_123456"}
+        yield func
+
+
+@pytest.fixture()
+def mock_submission():
+    submission = {
+        "model_repo": "ChaiML/test_model",
+        "developer_uid": "name",
+        "developer_key": "mock-key",
+        "generation_params": {
+            "temperature": 1.0,
+            "top_p": 1.0,
+            "top_k": 40,
+            "repetition_penalty": 1.0,
+        },
+        "formatter": "PygmalionFormatter",
+    }
+    return submission
+
+
+def test_client(mock_post, mock_submission):
+    response = submit_model(mock_submission)
+    expected_submission_id = "name_123456"
+    assert response == {"submission_id": expected_submission_id}
+
+
+def test_submit_client_posts_with_correct_payload(mock_post, mock_submission):
+    submit_model(mock_submission)
+    mock_post.assert_called_once_with(url=SUBMISSION_URL, json=mock_submission)
+
+
+def test_submit_client_posts_raises_for_failed_post(mock_post, mock_submission):
+    mock_post.return_value.status_code = 500
+    mock_post.return_value.json.return_value = {'error': 'some error'}
+    with pytest.raises(AssertionError) as e:
+        response = submit_model(mock_submission)
+    msg = "some error"
+    assert msg in str(e)
+
+
+def test_submit_client_posts_returns_correct_subimission_id(mock_post, mock_submission):
+    mock_post.return_value.json.return_value = {'submission_id': 'name_654321'}
+    response = submit_model(mock_submission)
+    expected_submission_id = "name_654321"
+    assert response == {"submission_id": expected_submission_id}
