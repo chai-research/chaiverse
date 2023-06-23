d9c9a0935299d97ae3ea99fda81e8d49b5bcc3d5
Alexandre Daoud <alex@seamlessml.com>
2023-06-23 19:02:14 +0000

    [4] Leaderboard in python package

--- guanaco_python_package/src/chai_guanaco/submit.py
+++ guanaco_python_package/src/chai_guanaco/submit.py
@@ -4,9 +4,11 @@ import os
 
 BASE_URL = "https://guanaco-submitter.chai-research.com"
 SUBMISSION_ENDPOINT = "/models/submit"
+ALL_SUBMISSION_STATUS_ENDPOINT = "/models/"
 INFO_ENDPOINT = "/models/{submission_id}"
 
 SUBMISSION_URL = BASE_URL + SUBMISSION_ENDPOINT
+MY_SUBMISSIONS_STATUS_URL = BASE_URL + ALL_SUBMISSION_STATUS_ENDPOINT
 INFO_URL = BASE_URL + INFO_ENDPOINT
 
 
@@ -16,7 +18,6 @@ def submit_model(model_submission: dict, developer_key: str):
         developer_key: str
         model_submission: dict
             model_repo: str - HuggingFace repo
-            developer_uid: str
             generation_params: dict
                 temperature: float
                 top_p: float
@@ -24,7 +25,7 @@ def submit_model(model_submission: dict, developer_key: str):
                 repetition_penalty: float
     """
     submission_url = get_submission_url()
-    headers = {'developer_key': developer_key}
+    headers = {'Authorization': f"Bearer {developer_key}"}
     response = requests.post(url=submission_url, json=model_submission, headers=headers)
     assert response.status_code == 200, response.json()
     return response.json()
@@ -32,10 +33,22 @@ def submit_model(model_submission: dict, developer_key: str):
 
 def get_model_info(submission_id, developer_key):
     url = get_info_url().format(submission_id=submission_id)
-    headers = {'developer_key': developer_key}
+    headers = {'Authorization': f"Bearer {developer_key}"}
     response = requests.get(url=url, headers=headers)
     assert response.status_code == 200, response.json()
     return response.json()
+
+
+def get_my_submissions(developer_key):
+    url = get_my_submissions_url()
+    headers = {'Authorization': f"Bearer {developer_key}"}
+    response = requests.get(url=url, headers=headers)
+    assert response.status_code == 200, response.json()
+    return response.json()
+
+
+def get_my_submissions_url():
+    return MY_SUBMISSIONS_STATUS_URL
 
 
 def get_submission_url():
