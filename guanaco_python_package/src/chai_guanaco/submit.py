3671775a2a6eeb4a04ec3126aa9546e7a5efa29e
Alexandre Daoud <alex.daoud@mac.com>
2023-06-26 15:48:05 -0700

    Public leaderboard

--- guanaco_python_package/src/chai_guanaco/submit.py
+++ guanaco_python_package/src/chai_guanaco/submit.py
@@ -1,17 +1,21 @@
 import requests
 import os
 
+import pandas as pd
+
 
 BASE_URL = "https://guanaco-submitter.chai-research.com"
 SUBMISSION_ENDPOINT = "/models/submit"
 ALL_SUBMISSION_STATUS_ENDPOINT = "/models/"
 INFO_ENDPOINT = "/models/{submission_id}"
 DEACTIVATE_ENDPOINT = "/models/{submission_id}/deactivate"
+LEADERBOARD_ENDPOINT = "/leaderboard"
 
 SUBMISSION_URL = BASE_URL + SUBMISSION_ENDPOINT
 MY_SUBMISSIONS_STATUS_URL = BASE_URL + ALL_SUBMISSION_STATUS_ENDPOINT
 INFO_URL = BASE_URL + INFO_ENDPOINT
 DEACTIVATE_URL = BASE_URL + DEACTIVATE_ENDPOINT
+LEADERBOARD_URL = BASE_URL + LEADERBOARD_ENDPOINT
 
 
 def submit_model(model_submission: dict, developer_key: str):
@@ -56,6 +60,43 @@ def deactivate_model(submission_id, developer_key):
     assert response.status_code == 200, response.json()
     print(response.json())
     return response.json()
+
+
+def display_leaderboard(developer_key: str):
+    leaderboard = get_leaderboard(developer_key)
+    df = pd.DataFrame(leaderboard).T
+    df.reset_index(inplace=True, drop=False)
+    df.index += 1
+    print(df)
+
+
+def get_leaderboard(developer_key: str):
+    headers = {
+        "developer_key": developer_key,
+    }
+    resp = requests.get(LEADERBOARD_URL, headers=headers)
+    assert resp.status_code == 200, resp.json()
+    leaderboard = resp.json()
+    leaderboard = _add_ratios_to_leaderboard(leaderboard)
+    leaderboard = _sort_leaderboard_by_ratio(leaderboard)
+    return leaderboard
+
+
+def _add_ratios_to_leaderboard(leaderboard):
+    for model, metrics in leaderboard.items():
+        assert "thumbs_up" in metrics.keys()
+        assert "thumbs_down" in metrics.keys()
+        total_feedbacks = metrics["thumbs_up"] + metrics["thumbs_down"]
+        ratio = metrics["thumbs_up"] / total_feedbacks
+        metrics["ratio"] = round(ratio, 2)
+    return leaderboard
+
+
+def _sort_leaderboard_by_ratio(leaderboard):
+    sort_key = lambda x: x[1]["ratio"]
+    sorted_leaderboard = sorted(leaderboard.items(), key=sort_key, reverse=True)
+    sorted_leaderboard = dict(sorted_leaderboard)
+    return sorted_leaderboard
 
 
 
