597464dfa67045ed18090fff6791a8daa1147f6e
Alexandre Daoud <alex@seamlessml.com>
2023-07-11 13:23:27 +0000

    Better end to end test

--- guanaco_python_package/tests/test_submit.py
+++ guanaco_python_package/tests/test_submit.py
@@ -51,7 +51,8 @@ def test_model_submitter(mock_submission, mock_post, mock_get_pending_to_success
     model_submitter._get_request_interval = 1
     submission_id = model_submitter.submit(mock_submission)
     headers = {"Authorization": "Bearer mock-key"}
-    mock_post.assert_called_once_with(url=submit.SUBMISSION_URL, json=mock_submission, headers=headers)
+    expected_url = submit.get_url(submit.SUBMISSION_ENDPOINT)
+    mock_post.assert_called_once_with(url=expected_url, json=mock_submission, headers=headers)
     assert mock_get_pending_to_success.call_count == 3
     assert submission_id == "name_123456"
 
@@ -65,7 +66,8 @@ def test_client(mock_post, mock_submission):
 def test_submit_client_posts_with_correct_payload(mock_post, mock_submission):
     submit.submit_model(mock_submission, developer_key="mock-key")
     headers={"Authorization": "Bearer mock-key"}
-    mock_post.assert_called_once_with(url=submit.SUBMISSION_URL, json=mock_submission, headers=headers)
+    expected_url = submit.get_url(submit.SUBMISSION_ENDPOINT)
+    mock_post.assert_called_once_with(url=expected_url, json=mock_submission, headers=headers)
 
 
 def test_submit_client_posts_raises_for_failed_post(mock_post, mock_submission):
@@ -92,8 +94,9 @@ def test_get_model_info(mock_get):
 
 def test_get_model_info_called_with_correct_url(mock_get):
     submit.get_model_info('name_123456', developer_key='key')
+    expected_url = submit.get_url(submit.INFO_ENDPOINT)
     mock_get.assert_called_once_with(
-        url=submit.INFO_URL.format(submission_id='name_123456'),
+        url=expected_url.format(submission_id='name_123456'),
         headers={"Authorization": "Bearer key"}
     )
 
@@ -110,8 +113,9 @@ def test_get_model_info_raises_with_error(mock_get):
 def test_get_my_submissions(mock_get):
     mock_get.return_value.json.return_value = {'123': 'pending', '456': 'failed'}
     out = submit.get_my_submissions('dev_key')
+    expected_url = submit.get_url(submit.ALL_SUBMISSION_STATUS_ENDPOINT)
     mock_get.assert_called_once_with(
-        url=submit.MY_SUBMISSIONS_STATUS_URL,
+        url=expected_url,
         headers={"Authorization": "Bearer dev_key"}
     )
     assert out == {'123': 'pending', '456': 'failed'}
@@ -131,7 +135,8 @@ def test_deactivate_model(mock_get):
     mock_get.return_value.json.return_value = ""
     submit.deactivate_model("test_model", developer_key="dev_key")
     expected_headers = {"Authorization": "Bearer dev_key"}
-    expected_url = submit.DEACTIVATE_URL.format(submission_id = "test_model")
+    expected_url = submit.get_url(submit.DEACTIVATE_ENDPOINT)
+    expected_url = expected_url.format(submission_id = "test_model")
     mock_get.assert_called_once_with(url=expected_url, headers=expected_headers)
 
 
